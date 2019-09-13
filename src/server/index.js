3288c71068e4c06c22b173a4dfc311b1e8bf6159
khalidhmd <khalidhmd@outlook.com>
2019-09-13 20:34:01 +0200

    initail commit with aylien configured

---
+++ src/server/index.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+const aylien = require("aylien_textapi");
+
+// set aylien API credentias
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
+
+const app = express()
+
+app.use(express.static('dist'))
+
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
