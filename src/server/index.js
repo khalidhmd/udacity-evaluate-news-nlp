78a3ad3f924bee8576950c910493bc2a03dd42b6
khalidhmd <khalidhmd@outlook.com>
2019-09-18 21:17:47 +0200

    complete webpack config

--- src/server/index.js
+++ src/server/index.js
@@ -17,14 +17,14 @@ app.use(express.static('dist'))
 
 
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
-})
+});
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
-})
+});
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,35 +1,30 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'development',
-    devtool: 'source-map',
-    stats: 'verbose',
+    entry: {
+        main: "./src/client/index.js"
+    },
+    mode: "development",
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html"
+        })
+    ],
     module: {
         rules: [
             {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
+                test: /\.scss$/,
+                use: [
+                    "style-loader", //3. Inject styles into DOM
+                    "css-loader", //2. Turns css into commonjs
+                    "sass-loader" //1. Turns sass into css
+                ]
             }
         ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
-    ]
-}
+    }
+};
