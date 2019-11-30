1785ccc902c7f4ef92cb417a1e88d456557d411d
khalidhmd <khalidhmd@outlook.com>
2019-11-30 22:23:00 +0200

    add env.PORT

--- src/server/index.js
+++ src/server/index.js
@@ -1,39 +1,39 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const path = require('path')
-const express = require('express')
+const path = require("path");
+const express = require("express");
 const aylien = require("aylien_textapi");
-const cors = require('cors')
+const cors = require("cors");
 
 // set aylien API credentias
 var textapi = new aylien({
-    application_id: process.env.API_ID,
-    application_key: process.env.API_KEY
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY,
 });
 
-const app = express()
-app.use(cors())
-app.use(express.static('dist'))
+const app = express();
+app.use(cors());
+app.use(express.static("dist"));
 
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'))
-})
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(process.env.PORT || 8080, function() {
+  console.log("Example app listening on port 8080!");
 });
 
-app.get('/test', function (req, res) {
-
-    textapi.sentiment({
-        'url': req.query.url
-    }, function (error, response) {
-        if (error === null) {
-            console.log('response', response);
-            res.send(response);
-        }
-    });
-
+app.get("/test", function(req, res) {
+  textapi.sentiment(
+    {
+      url: req.query.url,
+    },
+    function(error, response) {
+      if (error === null) {
+        console.log("response", response);
+        res.send(response);
+      }
+    },
+  );
 });
